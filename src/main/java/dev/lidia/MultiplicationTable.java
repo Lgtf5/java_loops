def3a83124649770e064651952ae367d37d32fe3
Lidia Glez-Tablas <lgtablasf5@gmail.com>
2025-03-27 09:05:13 +0100

    Revert "test commit"
    This reverts commit 7d893faf48f8b0efdcaf36e22fe9b8138fbcc9d0.

--- src/main/java/dev/lidia/MultiplicationTable.java
+++ src/main/java/dev/lidia/MultiplicationTable.java
@@ -1,15 +1,5 @@
 package dev.lidia;
 
-import com.sun.swing.internal.plaf.metal.resources.metal;
-
 public class MultiplicationTable {
-    public static int multiplicationTable(int size) {
-        int result = 0;
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                result += i * j;
-            }
-        }
-        return result;
-    }
+    
 }
