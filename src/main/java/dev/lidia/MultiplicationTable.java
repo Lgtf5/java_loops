b8cd916575c51a141b747e7b7ca59146b43b1bad
unknown <lgtablasf5@gmail.com>
2025-03-27 09:47:59 +0100

    test for multiplicationTable 2, susscessfull

--- src/main/java/dev/lidia/MultiplicationTable.java
+++ src/main/java/dev/lidia/MultiplicationTable.java
@@ -3,8 +3,12 @@ package dev.lidia;
 
 
 public class MultiplicationTable {
-    public static int multiplicationTable(int number) {
+    public static String multiplicationTable(int number) {
    
- 
+    String result = "";
+    for (int i = 1; i <= 10; i++) {
+        result += String.format ("%d x %d = %d\n", number, i, number * i);
     }
-}
+    return result;
+}}
+
--- src/test/java/dev/lidia/MultiplicationTableTest.java
+++ src/test/java/dev/lidia/MultiplicationTableTest.java
@@ -13,18 +13,18 @@ public class MultiplicationTableTest {
     @DisplayName("Test multiplicationTable")
     public void testMultiplicationTable() {
         int number = 2;
-        String resultTable = "2 x 1 = 2/n" +
-                            "2 x 2 = 4/n" +
-                            "2 x 3 = 6/n" +
-                            "2 x 4 = 8/n" +
-                            "2 x 5 = 10/n" +
-                            "2 x 6 = 12/n" +
-                            "2 x 7 = 14/n" +
-                            "2 x 8 = 16/n" +
-                            "2 x 9 = 18/n" +
-                            "2 x 10 = 20/n";
+        String resultTable = "2 x 1 = 2\n" +
+                            "2 x 2 = 4\n" +
+                            "2 x 3 = 6\n" +
+                            "2 x 4 = 8\n" +
+                            "2 x 5 = 10\n" +
+                            "2 x 6 = 12\n" +
+                            "2 x 7 = 14\n" +
+                            "2 x 8 = 16\n" +
+                            "2 x 9 = 18\n" +
+                            "2 x 10 = 20\n";
 
-        String result = table.multiplicationTable(number);
+        String result = MultiplicationTable.multiplicationTable(number);
         assertThat(result, is(resultTable));     
     }
 }
