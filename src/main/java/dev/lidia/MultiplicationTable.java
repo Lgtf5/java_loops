d2c66298e3d564f36b1de7d350601de819fb29c0
unknown <lgtablasf5@gmail.com>
2025-03-27 09:12:45 +0100

    first test successfull

--- src/main/java/dev/lidia/MultiplicationTable.java
+++ src/main/java/dev/lidia/MultiplicationTable.java
@@ -1,5 +1,10 @@
 package dev.lidia;
 
+
+
 public class MultiplicationTable {
-    
+    public static int multiplicationTable(int number) {
+        return Integer.valueOf(number);
+ 
+    }
 }
--- src/test/java/dev/lidia/MultiplicationTableTest.java
+++ src/test/java/dev/lidia/MultiplicationTableTest.java
@@ -1,11 +1,13 @@
 package dev.lidia;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class MultiplicationTableTest {
+
+   
     
     @Test
     @DisplayName("Test 1")
